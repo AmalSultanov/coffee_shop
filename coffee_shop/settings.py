caaa0b2b7540e15c56ef832048f7213f5720f5c1
Sultanov Amal <sultanovamal@MacBook-Air-Sultanov.local>
2024-07-18 21:21:08 +0500

    removed redundant comments

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -40,9 +40,7 @@ ROOT_URLCONF = 'coffee_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'templates'
-        ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
